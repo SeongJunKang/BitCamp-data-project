5ef78db426c9de971e68b5037f83ca5833d9b284
SeongJunKang <xlsxlsdh@nate.com>
2016-06-23 17:07:50 +0900

    .

--- bitproject/src/main/java/bitcamp/pet/controller/ajax/MemberAjaxController.java
+++ bitproject/src/main/java/bitcamp/pet/controller/ajax/MemberAjaxController.java
@@ -1,9 +1,11 @@
 package bitcamp.pet.controller.ajax;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.google.gson.Gson;
 
@@ -86,6 +91,8 @@ public class MemberAjaxController {
     result.put("name",member.getName());
     result.put("email",member.getEmail());
     result.put("tel",member.getTel());
+    result.put("gra",member.getGra());
+    result.put("pwd",member.getPwd());
     try {
       result.put("prof",member.getProf());
     } catch (Exception e) {
@@ -129,10 +136,28 @@ public class MemberAjaxController {
   @RequestMapping(method=RequestMethod.POST,
       produces="application/json;charset=UTF-8", value="update")
   @ResponseBody
-  public String update(int no, String name, String password, String tel, int gra, String prof)
+  public String update(/*MultipartHttpServletRequest request,*/ int no, String name, String password, String tel, int gra)
       throws ServletException, IOException {
-
     Member member = memberService.retrieveByNo(no);
+    /*
+    Map<String, MultipartFile> files = request.getFileMap();
+    CommonsMultipartFile cmf = (CommonsMultipartFile) files.get("uploadFile");
+    int extPoint = cmf.getOriginalFilename().lastIndexOf(".");
+    String fileName = System.currentTimeMillis()+count()
+                        + cmf.getOriginalFilename().substring(extPoint);
+    // 경로
+    String path ="bitproject/img/profiles/"+fileName;
+
+    // 파일 업로드 처리 완료.
+    try {
+      cmf.transferTo(new File(path));
+      System.out.println("새 파일명 : " + fileName);
+      System.out.printf("새 파일을 저장할 실제 경로 = %s\n", path);
+      member.setProf(path);
+    } catch (Exception e) {
+    }*/
+    
+    
     if (!password.equals("")){
       member.setPwd(password);
     }
@@ -141,12 +166,6 @@ public class MemberAjaxController {
     }
     if (!tel.equals("")) {
       member.setTel(tel);
-    }
-    try {
-      if (!prof.equals("") || (prof != null)) {
-        member.setProf(prof);
-      }
-    } catch (Exception e) {
     }
     HashMap<String, Object> result = new HashMap<>();
     try {
@@ -158,5 +177,13 @@ public class MemberAjaxController {
     }
     return new Gson().toJson(result);
   }
+  
+  int countno = 0;
 
+  synchronized private int count() {
+    int i = ++countno;
+    if (i == 1000)
+      i = 1;
+    return i;
+  }
 }
